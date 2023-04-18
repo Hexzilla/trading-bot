0d26e11078373b4918d88460d8857e8e5d169339
Hexzilla <hexzilla256@gmail.com>
2023-04-18 19:10:46 +0300

    update place order method

--- brokers/tda/order_handlers/tda_order_processor.py
+++ brokers/tda/order_handlers/tda_order_processor.py
@@ -1,5 +1,9 @@
 import logging
 
+from tda.client import Client
+from tda.orders.common import Session, Duration
+from tda.orders.equities import equity_buy_market, equity_sell_market
+
 from brokers.tda.broker_client import BrokerClient
 from core.order_handlers.order_processor import OrderProcessor
 from core.signal import Signal
@@ -14,7 +18,20 @@ class TdaOrderProcessor(OrderProcessor):
         self.logger.warning('Caught algo signal: ' + signal.__str__())
 
     def place_order(self, account_id: str, ticker: str):
-        pass
+        tda_client: Client = self.broker_client._get_client()
+
+        order_type = 'buy'
+        shares = 1
+
+        if order_type == 'buy':
+            order_spec = equity_buy_market(ticker, shares).set_session(
+                Session.NORMAL).set_duration(Duration.DAY).build()
+            tda_client.place_order(account_id, order_spec)
+
+        if order_type == 'sell':
+            order_spec = equity_sell_market(ticker, shares).set_session(
+                Session.NORMAL).set_duration(Duration.DAY).build()
+            tda_client.place_order(account_id, order_spec)
 
     def replace_order(self, account_id: str, order_id: str):
         pass
