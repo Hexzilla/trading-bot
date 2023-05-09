b73eca412be6a44e15fc180642b37f0519cc0ff3
Hexzilla <hexzilla256@gmail.com>
2023-05-09 17:01:27 +0300

    add db_account module

---
+++ brokers/tda/db/db_account.py
@@ -0,0 +1,41 @@
+from db import sqlite
+
+
+def insert_account(account):
+    sql = ''' INSERT INTO account_info (user_id, enabled_user) VALUES(?,?) '''
+    return sqlite.insert(account, sql)
+
+
+def update_account(account):
+    sql = '''UPDATE account_info SET user_id = ?, enabled_user = ? WHERE user_id = ?'''
+    sqlite.update(account, sql)
+
+
+def select_all():
+    sql = 'SELECT * from account_info'
+    return sqlite.select_all(sql)
+
+
+def select_by_user_id(user_id):
+    sql = 'SELECT * from account_info WHERE user_id = ?'
+    return sqlite.select_by_key(user_id, sql)
+
+
+def select_by_enabled_user(enabled_user):
+    sql = 'SELECT * from account_info WHERE enabled_user = ?'
+    return sqlite.select_by_key(enabled_user, sql)
+
+
+def delete_all():
+    sql = 'DELETE from account_info'
+    sqlite.delete_all(sql)
+
+
+def delete_by_user_id(user_id):
+    sql = 'DELETE FROM account_info WHERE user_id = ?'
+    sqlite.delete_by_key(user_id, sql)
+
+
+def delete_by_enabled_user(enabled_user):
+    sql = 'DELETE FROM account_info WHERE enabled_user = ?'
+    sqlite.delete_by_key(enabled_user, sql)
